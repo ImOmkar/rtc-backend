702f0f7f3c4e5d190d74cc3efc3f2bb2878250e0
ImOmkar <omkar211196@gmail.com>
2025-02-07 11:42:57 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,77 @@
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+// const cors = require("cors");
+
+// const app = express();
+// app.use(cors({ origin: "*" }));
+
+// const server = http.createServer(app);
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "*",  // Allow all origins (for development)
+//   },
+// });
+
+// const usersInRoom = {}; // Track users in each room
+
+// io.on("connection", (socket) => {
+//   console.log("User connected:", socket.id);
+
+//   socket.on("join-room", ({ roomName, userName }) => {
+//     socket.join(roomName);
+
+//     if (!usersInRoom[roomName]) {
+//       usersInRoom[roomName] = [];
+//     }
+
+//     // Notify new user about existing users in the room
+//     const existingUsers = usersInRoom[roomName];
+//     socket.emit("all-users", existingUsers);
+
+//     // Store new user in room
+//     usersInRoom[roomName].push({ id: socket.id, userName });
+
+//     // Notify all users that a new user joined
+//     socket.broadcast.to(roomName).emit("user-connected", { userId: socket.id, name: userName });
+
+//     console.log(`${userName} joined the room: ${roomName}`);
+
+//   });
+
+//   socket.on("sending-signal", ({ userToSignal, callerId, signal }) => {
+//     io.to(userToSignal).emit("receive-signal", { signal, callerId });
+//   });
+
+//   socket.on("returning-signal", ({ signal, callerId }) => {
+//     io.to(callerId).emit("receive-signal", { signal, callerId });
+//   });
+
+//   socket.on("leave-room", ({ roomName }) => {
+//     socket.leave(roomName);
+//     for (let roomName in usersInRoom) {
+//         if (usersInRoom[roomName]) {
+//           usersInRoom[roomName] = usersInRoom[roomName].filter((user) => user.id !== socket.id);
+  
+//           // Notify the room that the user has disconnected.
+//           socket.to(roomName).emit('user-disconnected', socket.id);
+//         }
+//     }    
+//     io.to(roomName).emit("user-disconnected", socket.id);
+//   });
+
+//   socket.on("disconnect", () => {
+//     for (let room in usersInRoom) {
+//       usersInRoom[room] = usersInRoom[room].filter((user) => user.id !== socket.id);
+//       io.to(room).emit("user-disconnected", { userId: socket.id, name: userName });
+//     }
+//   });
+  
+// });
+
+  
+// server.listen(5000, () => console.log("Server running on port 5000"));
+
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -9,7 +83,7 @@ app.use(cors({ origin: "*" }));
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",  // Allow all origins (for development)
+    origin: "*", // Allow all origins (for development)
   },
 });
 
@@ -36,7 +110,6 @@ io.on("connection", (socket) => {
     socket.broadcast.to(roomName).emit("user-connected", { userId: socket.id, name: userName });
 
     console.log(`${userName} joined the room: ${roomName}`);
-
   });
 
   socket.on("sending-signal", ({ userToSignal, callerId, signal }) => {
@@ -49,25 +122,29 @@ io.on("connection", (socket) => {
 
   socket.on("leave-room", ({ roomName }) => {
     socket.leave(roomName);
-    for (let roomName in usersInRoom) {
-        if (usersInRoom[roomName]) {
-          usersInRoom[roomName] = usersInRoom[roomName].filter((user) => user.id !== socket.id);
-  
-          // Notify the room that the user has disconnected.
-          socket.to(roomName).emit('user-disconnected', socket.id);
-        }
-    }    
-    io.to(roomName).emit("user-disconnected", socket.id);
+
+    if (usersInRoom[roomName]) {
+      usersInRoom[roomName] = usersInRoom[roomName].filter((user) => user.id !== socket.id);
+
+      // Notify the room that the user has disconnected.
+      io.to(roomName).emit("user-disconnected", { userId: socket.id });
+    }
   });
 
   socket.on("disconnect", () => {
     for (let room in usersInRoom) {
-      usersInRoom[room] = usersInRoom[room].filter((user) => user.id !== socket.id);
-      io.to(room).emit("user-disconnected", { userId: socket.id, name: userName });
+      const userIndex = usersInRoom[room].findIndex((user) => user.id === socket.id);
+      if (userIndex !== -1) {
+        const userName = usersInRoom[room][userIndex].userName;
+        usersInRoom[room].splice(userIndex, 1);
+
+        // Notify the room that the user has disconnected.
+        io.to(room).emit("user-disconnected", { userId: socket.id, name: userName });
+      }
     }
   });
-  
 });
 
-  
-server.listen(5000, () => console.log("Server running on port 5000"));
+// Use Railway-provided PORT
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
